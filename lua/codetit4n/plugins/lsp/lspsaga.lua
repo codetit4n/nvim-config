2f759c910ca2034e61ef4cccc20c8d385c4c48e1
codeTIT4N <lokeshkumar636@yahoo.com>
2023-07-03 02:18:55 +0530

    fixed hover issue saga

--- lua/codetit4n/plugins/lsp/lspsaga.lua
+++ lua/codetit4n/plugins/lsp/lspsaga.lua
@@ -1,19 +1,28 @@
 -- import lspsaga safely
 local saga_status, saga = pcall(require, "lspsaga")
 if not saga_status then
-  return
+	return
 end
 
 saga.setup({
-  -- keybinds for navigation in lspsaga window
-  scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
-  -- use enter to open file with definition preview
-  definition = {
-    edit = "<CR>",
-  },
-  ui = {
-    colors = {
-      normal_bg = "#022746",
-    },
-  },
+	-- keybinds for navigation in lspsaga window
+	move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+	-- use enter to open file with finder
+	finder_action_keys = {
+		open = "<CR>",
+	},
+	-- use enter to open file with definition preview
+	definition_action_keys = {
+		edit = "<CR>",
+	},
+	symbol_in_winbar = {
+		enable = true,
+		separator = " > ",
+		ignore_patterns = {},
+		hide_keyword = true,
+		show_file = true,
+		folder_level = 2,
+		respect_root = false,
+		color_mode = true,
+	},
 })
