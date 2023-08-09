de9159cca595faa3e262bd593b6340a1b1557213
codeTIT4N <lokeshkumar636@yahoo.com>
2023-08-10 04:43:46 +0530

    colorscheme

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,4 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>t', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.git_files, {})
+vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
--- lua/codetit4n/packer.lua
+++ lua/codetit4n/packer.lua
@@ -11,6 +11,8 @@ return require('packer').startup(function(use)
    'nvim-telescope/telescope.nvim', tag = '0.1.2',
    -- or                            , branch = '0.1.x',
    requires = { {'nvim-lua/plenary.nvim'} }
-}
+  }
+
+   use 'marko-cerovac/material.nvim'
 
 end)
