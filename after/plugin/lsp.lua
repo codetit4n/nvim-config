8f41d9c4269770b4124294a49adbca16734630f9
codeTIT4N <lokeshkumar636@yahoo.com>
2023-08-12 20:11:48 +0530

    small fixes lsp and treesitter

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,17 +1,16 @@
 local lsp = require('lsp-zero').preset({})
+local lspconfig = require('lspconfig')
 
 lsp.ensure_installed({
   "tsserver",
   "rust_analyzer",
   "html",
   "cssls",
-  "tailwindcss",
   "lua_ls",
   "marksman",
   "yamlls",
   "jedi_language_server",
   "clangd",
-  "csharp_ls",
   "docker_compose_language_service",
   "dockerls",
   "move_analyzer",
@@ -38,23 +37,135 @@ lsp.setup_nvim_cmp({
   mapping = cmp_mappings
 })
 
-lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
+-- enable keybinds only for when lsp server available
+local on_attach = function(client, bufnr)
+	-- keybind options
+	local opts = { noremap = true, silent = true, buffer = bufnr }
+
+	-- set keybinds
+	-- vim.keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
+	vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
+	-- vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+	vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+	-- vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+	-- vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+	-- vim.keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+	-- vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
+    -- vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
+	-- vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+	-- vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+
+end
+
+lspconfig["lua_ls"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = { -- custom settings for lua
+		Lua = {
+			-- make the language server recognize "vim" global
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				-- make language server aware of runtime files
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+		},
+	},
+})
+
+lspconfig["tsserver"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+
+lspconfig["rust_analyzer"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	cmd = {
+		"rustup",
+		"run",
+		"stable",
+		"rust-analyzer",
+	},
+	settings = {
+		["rust-analyzer"] = {
+			cargo = {
+				allFeatures = true,
+			},
+		},
+	},
+})
+
+-- configure html server
+lspconfig["html"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach, -- for keybinds
+})
 
 
--- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+lspconfig["cssls"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+})
+
+lspconfig["jedi_language_server"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	single_file = true,
+})
+
+-- configure marksman server
+lspconfig["marksman"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach, -- for keybinds
+})
+
+lspconfig["yamlls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach, -- for keybinds
+})
+
+-- configure c++ language server
+-- local capabilitiesClangd = vim.lsp.protocol.make_client_capabilities()
+-- capabilitiesClangd.offsetEncoding = { "utf-16" }
+lspconfig["clangd"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+})
+
+-- configure docker-compose language server
+lspconfig["docker_compose_language_service"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	filetypes = { "yaml", "yml" },
+	root_dir = lspconfig.util.root_pattern("docker-compose.yml", "docker-compose.yaml"),
+	single_file_support = true,
+})
+
+-- configure dockerfile language server
+lspconfig["dockerls"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	filetypes = { "Dockerfile", "dockerfile" },
+	root_dir = lspconfig.util.root_pattern("Dockerfile", "dockerfile"),
+	single_file_support = true,
+})
+
+-- configure move_analyzer language server
+lspconfig["move_analyzer"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+})
+
+lspconfig["solidity"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
 
 lsp.setup()
