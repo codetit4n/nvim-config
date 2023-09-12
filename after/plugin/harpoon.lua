0e966d9e9d544866edd99c3a1afc5d31b3ce7c5d
codeTIT4N <lokeshkumar636@yahoo.com>
2023-09-12 15:32:10 +0530

    fixed conflicting keymaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,8 +1,8 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>ha", ui.toggle_quick_menu, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>m", mark.add_file, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ha", ui.toggle_quick_menu, { silent = true })
+vim.keymap.set("n", "<leader>m", mark.add_file, { silent = true })
 
-vim.keymap.set("n", "<C-l>", ui.nav_next, { noremap = true, silent = true })
-vim.keymap.set("n", "<C-m>", ui.nav_prev, { noremap = true, silent = true })
+vim.keymap.set("n", "<C-p>", ui.nav_prev, { silent = true })
+vim.keymap.set("n", "<C-n>", ui.nav_next, { silent = true })
