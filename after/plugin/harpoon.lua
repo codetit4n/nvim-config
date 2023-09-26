236146b27263c006e77f235c2ea9a8518ee5db39
codeTIT4N <lokeshkumar636@yahoo.com>
2023-09-27 01:35:52 +0530

    small remap

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,7 +1,7 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>ha", ui.toggle_quick_menu, { silent = true })
+vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu, { silent = true })
 vim.keymap.set("n", "<leader>m", mark.add_file, { silent = true })
 
 vim.keymap.set("n", "<C-p>", ui.nav_prev, { silent = true })
