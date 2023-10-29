38c2fb71d065ce727781b17c624975ffe6dcb12c
Lokesh Kumar <lokeshkumar636@yahoo.com>
2023-10-30 02:54:34 +0530

    colorscheme and lualine updated

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,5 +1,5 @@
 function ColorMyScreen(color)
-	color = color or "base16-circus"
+	color = color or "base16-monokai"
 	-- color = color or "github_dark_tritanopia"
 	vim.cmd.colorscheme(color)
 
@@ -7,7 +7,7 @@ function ColorMyScreen(color)
 	vim.api.nvim_set_hl(0, "Cursor", { bg = "none" })
 
 	vim.cmd("highlight LineNr guibg=none")
-	vim.cmd("highlight LineNr guifg=none")
+	vim.cmd("highlight LineNr guifg=#a8a6a5")
 	vim.cmd("highlight SignColumn guibg=none")
 end
 ColorMyScreen()
