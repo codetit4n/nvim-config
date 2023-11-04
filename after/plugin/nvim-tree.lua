7c2d2bded05b00f101b6a552f522896f013c403a
Lokesh Kumar <lokeshkumar636@yahoo.com>
2023-11-04 22:50:45 +0530

    small nvim tree color fix

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -72,10 +72,10 @@ end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 -- change the colours of everything according to the theme
-vim.cmd([[ highlight NvimTreeCursorLine guifg=red ]])
+vim.cmd([[ highlight NvimTreeCursorLine guifg=none ]])
 vim.cmd([[ highlight NvimTreeCursorLine guibg=none ]])
 vim.cmd([[ highlight NvimTreeCursorLineNr guibg=none ]])
-vim.cmd([[ highlight NvimTreeRootFolder guifg=#afaf87 ]])
+vim.cmd([[ highlight NvimTreeRootFolder guifg=none ]])
 --vim.cmd([[ highlight NvimTreeFolderIcon guifg=#afaf87 ]])
 --vim.cmd([[ highlight NvimTreeFolderName guifg=lightgrey ]])
 --vim.cmd([[ highlight NvimTreeEmptyFolderName guifg=lightgrey ]])
