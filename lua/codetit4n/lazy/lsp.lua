8f7bb56336ddd3b3bb75577e61c26f78cb7e72b1
Lokesh Kumar [codetit4n] <hi@loke.sh>
2026-08-25 17:13:54 +0530

    housekeeping, waka added

--- lua/codetit4n/lazy/lsp.lua
+++ lua/codetit4n/lazy/lsp.lua
@@ -2,7 +2,7 @@ return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
 		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
+		"mason-org/mason-lspconfig.nvim",
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
@@ -13,12 +13,15 @@ return {
 	},
 
 	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 		local function get_highest_version(versions)
 			table.sort(versions, function(a, b)
 				local function version_to_tuple(v)
 					local major, minor, patch = v:match("(%d+)%.(%d+)%.?(%d*)")
 					return tonumber(major), tonumber(minor), tonumber(patch or "0")
 				end
+
 				local a1, a2, a3 = version_to_tuple(a)
 				local b1, b2, b3 = version_to_tuple(b)
 
@@ -31,12 +34,13 @@ return {
 				end
 			end)
 
-			return versions[#versions] -- highest version
+			return versions[#versions]
 		end
 
 		local function parse_remappings()
 			local remappings = {}
 			local remappings_file = vim.fn.findfile("remappings.txt", ".;")
+
 			if remappings_file == "" then
 				return remappings
 			end
@@ -47,11 +51,16 @@ return {
 					remappings[key] = value
 				end
 			end
+
 			return remappings
 		end
 
 		local function get_solidity_version()
-			local foundry_toml = vim.fs.find("foundry.toml", { upward = true, type = "file" })
+			local foundry_toml = vim.fs.find("foundry.toml", {
+				upward = true,
+				type = "file",
+			})
+
 			if #foundry_toml > 0 then
 				for line in io.lines(foundry_toml[1]) do
 					local version = line:match('solc_version%s*=%s*"(.-)"')
@@ -61,17 +70,23 @@ return {
 				end
 			end
 
-			local hardhat_config = vim.fs.find(
-				{ "hardhat.config.js", "hardhat.config.ts" },
-				{ upward = true, type = "file" }
-			)
+			local hardhat_config = vim.fs.find({
+				"hardhat.config.js",
+				"hardhat.config.ts",
+			}, {
+				upward = true,
+				type = "file",
+			})
+
 			if #hardhat_config > 0 then
 				local versions = {}
+
 				for line in io.lines(hardhat_config[1]) do
 					for version in line:gmatch('version:%s*"(.-)"') do
 						table.insert(versions, version)
 					end
 				end
+
 				if #versions > 0 then
 					return get_highest_version(versions)
 				end
@@ -80,14 +95,123 @@ return {
 			return "latest"
 		end
 
-		-- Setup
 		local border = "rounded"
+
 		local handlers = {
 			["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+
 			["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
 		}
 
+		-- Default capabilities/handlers inherited by every server
+		vim.lsp.config("*", {
+			capabilities = capabilities,
+			handlers = handlers,
+		})
+
+		-- Lua
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					runtime = {
+						version = "Lua 5.1",
+					},
+					diagnostics = {
+						globals = {
+							"vim",
+							"it",
+							"describe",
+							"before_each",
+							"after_each",
+						},
+					},
+				},
+			},
+		})
+
+		-- Rust
+		vim.lsp.config("rust_analyzer", {
+			settings = {
+				["rust-analyzer"] = {
+					cargo = {
+						buildScripts = {
+							enable = true,
+						},
+					},
+				},
+			},
+		})
+
+		-- Solidity
+		vim.lsp.config("solidity_ls", {
+			cmd = {
+				"vscode-solidity-server",
+				"--stdio",
+			},
+
+			filetypes = {
+				"solidity",
+			},
+
+			root_markers = {
+				"foundry.toml",
+				"hardhat.config.js",
+				"hardhat.config.ts",
+			},
+
+			settings = {
+				solidity = {
+					compileUsingRemoteVersion = get_solidity_version(),
+					defaultCompiler = "remote",
+					enabledAsYouTypeCompilationErrorCheck = true,
+					remapping = parse_remappings(),
+				},
+			},
+		})
+
+		-- Aptos Move
+		vim.lsp.config("aptos_move_analyzer", {
+			cmd = {
+				"aptos-move-analyzer",
+			},
+
+			filetypes = {
+				"move",
+			},
+
+			root_markers = {
+				"Move.toml",
+			},
+
+			settings = {},
+		})
+
+		-- Fuel Sway
+		vim.lsp.config("sway_lsp", {
+			cmd = {
+				"forc-lsp",
+			},
+
+			filetypes = {
+				"sway",
+			},
+
+			root_markers = {
+				"Forc.toml",
+			},
+
+			init_options = {
+				logging = {
+					level = "trace",
+				},
+			},
+
+			settings = {},
+		})
+
+		-- Mason
 		require("mason").setup()
+
 		require("mason-lspconfig").setup({
 			ensure_installed = {
 				"lua_ls",
@@ -103,111 +227,13 @@ return {
 				"cmake",
 				"yamlls",
 			},
-			handlers = {
-				function(server_name)
-					require("lspconfig")[server_name].setup({
-						capabilities = Capabilities,
-						handlers = handlers,
-					})
-				end,
-				["solidity_ls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.solidity_ls.setup({
-						cmd = { "vscode-solidity-server", "--stdio" },
-						filetypes = { "solidity" },
-						root_dir = lspconfig.util.root_pattern("foundry.toml", "hardhat.config.*"),
-						settings = {
-							solidity = {
-								compileUsingRemoteVersion = get_solidity_version(),
-								defaultCompiler = "remote",
-								enabledAsYouTypeCompilationErrorCheck = true,
-								remapping = parse_remappings(),
-							},
-						},
-						capabilities = Capabilities,
-						handlers = handlers,
-					})
-				end,
-				["lua_ls"] = function()
-					require("lspconfig").lua_ls.setup({
-						capabilities = Capabilities,
-						handlers = handlers,
-						settings = {
-							Lua = {
-								runtime = { version = "Lua 5.1" },
-								diagnostics = {
-									globals = { "vim", "it", "describe", "before_each", "after_each" },
-								},
-							},
-						},
-					})
-				end,
-			},
+			automatic_enable = true,
 		})
 
-		-- Rust analyzer (separately)
-		require("lspconfig").rust_analyzer.setup({
-			capabilities = Capabilities,
-			handlers = handlers,
-			settings = {
-				["rust-analyzer"] = {
-					cargo = {
-						buildScripts = { enable = true },
-					},
-				},
-			},
-		})
-
-		-- Custom LSPs
-		local lspconfig = require("lspconfig")
-		local configs = require("lspconfig.configs")
-
-		-- Aptos Move
-
-		-- Aptos Move language server
-		-- install aptos move analyzer - cargo install --git https://github.com/movebit/move --branch feature/aptos_move_analyzer aptos-move-analyzer
-		-- (last properly working version: 0.0.3)
-		-- In case the branch is not available, try: cargo install --git https://github.com/codetit4n/move --branch feature/aptos_move_analyzer aptos-move-analyzer
-		-- which is a fork of the original repository
-
-		if not configs.aptos_move_analyzer then
-			configs.aptos_move_analyzer = {
-				default_config = {
-					cmd = { "aptos-move-analyzer" },
-					filetypes = { "move" },
-					root_dir = function(fname)
-						return vim.fs.dirname(vim.fs.find({ "Move.toml" }, { upward = true })[1])
-					end,
-					settings = {},
-				},
-			}
-		end
-
-		lspconfig.aptos_move_analyzer.setup({
-			capabilities = Capabilities,
-			handlers = handlers,
-		})
-
-		-- Fuel Sway
-		if not configs.sway_lsp then
-			configs.sway_lsp = {
-				default_config = {
-					cmd = { "forc-lsp" },
-					filetypes = { "sway" },
-					init_options = {
-						logging = { level = "trace" },
-					},
-					root_dir = function(fname)
-						return vim.fs.dirname(vim.fs.find({ "Forc.toml" }, { upward = true })[1])
-					end,
-					settings = {},
-				},
-			}
-		end
-
-		lspconfig.sway_lsp.setup({
-			capabilities = Capabilities,
-			handlers = handlers,
+		-- Custom/non-Mason servers
+		vim.lsp.enable({
+			"aptos_move_analyzer",
+			"sway_lsp",
 		})
 	end,
 }
--- lua/codetit4n/lazy/treesitter.lua
+++ lua/codetit4n/lazy/treesitter.lua
@@ -1,9 +1,11 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
 	build = ":TSUpdate",
+	lazy = false,
+
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			-- A list of parser names, or "all"
 			ensure_installed = {
 				"vimdoc",
 				"javascript",
@@ -18,11 +20,7 @@ return {
 				"gitignore",
 			},
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
 			sync_install = false,
-
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
 			auto_install = true,
 
 			indent = {
@@ -30,21 +28,16 @@ return {
 			},
 
 			highlight = {
-				-- `false` will disable the whole extension
 				enable = true,
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
 				additional_vim_regex_highlighting = { "markdown" },
 			},
 		})
 
 		local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
 		treesitter_parser_config.move = {
 			install_info = {
-				url = "https://github.com/codeTIT4N/tree-sitter-move", -- forked from https://github.com/yangzq12/tree-sitter-move
+				url = "https://github.com/codeTIT4N/tree-sitter-move",
 				files = { "src/parser.c" },
 				branch = "master",
 			},
